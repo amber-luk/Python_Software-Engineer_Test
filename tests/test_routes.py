b300bb5825618413da804de3929d16f224435f27
amber-luk <47549276+amber-luk@users.noreply.github.com>
2021-08-04 02:07:25 -0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,17 +3,13 @@ from flask import url_for
 
 from app import app
 
-
-# @pytest.fixture
-# def client():
-#     app = create_app()
-#     return app.test_client()
-
+# testing original route
 def test_total():
     response = app.test_client().get("/total")
     assert response.status_code == 200
     assert response.data == b'{"total":50000005000000}\n'
 
+# testing second route
 def test_total2(): 
     response = app.test_client().get("/total2")
     assert response.status_code == 200
